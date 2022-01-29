d5f1a450bbaa3ba7f905f65ebcc4d4a263037cb1
xpushpeshx <brahmapushpesh@gmail.com>
2022-01-29 12:47:07 +0530

    ok done

--- website/models.py
+++ website/models.py
@@ -1,9 +1,14 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+def custom_validate_email(value):
+    if len(email)<8:
+        raise ValidationError('Email is incorrect')
 
 class Contact(models.Model):
     ip=models.GenericIPAddressField(null=True)
     name= models.CharField(max_length = 500)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(max_length=254 , blank=False , validators=[custom_validate_email])
     subject = models.TextField()
     message= models.TextField()
 
